2e5b9f742c77e5a140d0b113cc41979aed9113b6
Bryan Marty <bxm156@case.edu>
2011-12-31 18:20:35 -0500

    Added registration confirmation

--- ServicePad/apps/users/urls.py
+++ ServicePad/apps/users/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import redirect_to
 
 urlpatterns = patterns('ServicePad.apps.users.views',
     (r'^$', 'register'),
-   #(r'^confirm/$','register')
+    (r'^confirm/$',redirect_to, {'url': '/register', 'permanent': False}),
+    (r'^confirm/(?P<user>[0-9]+)/(?P<key>[a-zA-Z0-9]{32})/$','confirm')
 )
--- ServicePad/apps/users/views.py
+++ ServicePad/apps/users/views.py
@@ -3,7 +3,8 @@ import hashlib, random, datetime
 from forms import RegistrationForm
 from models import Volunteer
 
-from django.shortcuts import render_to_response, RequestContext
+from django.shortcuts import render_to_response, RequestContext, redirect, get_object_or_404
+from django.core.exceptions import MultipleObjectsReturned
 
 def register(request):    
     if request.POST:
@@ -11,7 +12,7 @@ def register(request):
         registration = RegistrationForm(new_data)
         if registration.is_valid():
             #Create user
-            new_user = registration.save();
+            new_user = registration.save()
             
             #Email confirmation
             salt = str(random.random())
@@ -23,14 +24,39 @@ def register(request):
                                     key_expires=key_expires
                         )
             new_profile.save()
+            url = request.get_host() + "/register/confirm/%u/%s" % (new_user.id,activation_key)
+            return render_to_response('users/register_thankyou.html',{'url':url})
         else:
             context = RequestContext(request,
                                      {'errors':registration.errors,
                                      'form':registration})
-            return render_to_response('users/register.html',context)
+            return render_to_response('users/register.html', context)
 
     registration = RegistrationForm()
     context = RequestContext(request,
            {'form':registration}
     )
     return render_to_response('users/register.html', context)
+
+def confirm(request, user, key):
+    if request.user.is_authenticated():
+        #Users is logged in and already confirmed
+        return redirect("/account")
+    #Verify the confirmation request
+    try:
+        user_profile = get_object_or_404(Volunteer, id=user, activation_key=key)
+    except MultipleObjectsReturned:
+        return render_to_response('users/confirm.html', {'success':False})
+        
+    if user_profile.key_expires < datetime.datetime.today():
+        #Resend the confirmation email with a new confirmation challenge
+        return render_to_response('users/confirm.html', {'expired':True})
+    #If valid
+    user_account = user_profile.user
+    if user_account.is_active:
+        #User was already activated
+        return redirect("/")
+    #Activate User
+    user_account.is_active = True
+    user_account.save()
+    return render_to_response('users/confirm.html', {'success':True})
