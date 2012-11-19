aa1ec725c1d700d8546135296b51d926f8f45ee1
Owen Bell <owenalexbell@gmail.com>
2012-11-18 21:48:58 -0500

    Rudimentary version of listing all events in the service

--- ServicePad/apps/events/urls.py
+++ ServicePad/apps/events/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns('ServicePad.apps.events.views',
     (r'^(?P<id>[0-9]+)/?$', 'view'),
-    (r'^create/?$','create')
+    (r'^create/?$','create'),
+	(r'^', 'list')
 )
