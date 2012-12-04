3cb3334184330bdf3fb5d5addd29f221019e2b8e
Bryan Marty <bxm156@case.edu>
2012-12-04 16:30:31 -0500

    Graphical improvements

--- ServicePad/apps/messages/forms.py
+++ ServicePad/apps/messages/forms.py
@@ -3,6 +3,10 @@ from models import Message
     
 class ComposeMessageForm(forms.ModelForm):
     
+    def __init__(self, *args, **kwargs):
+        super(ComposeMessageForm, self).__init__(*args, **kwargs)
+        self.fields['toUser'].label = "To"
+    
     class Meta:
         model = Message
         exclude = ('fromUser')
