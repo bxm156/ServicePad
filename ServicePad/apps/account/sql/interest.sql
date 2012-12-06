30cccd303d820ccb36cfaee12453255e92960f35
Bryan Marty <bxm156@case.edu>
2012-12-06 13:02:55 -0500

    More intial values

---
+++ ServicePad/apps/account/sql/interest.sql
@@ -0,0 +1,7 @@
+INSERT INTO account_interest (id,name) VALUES (1,'Web Design');
+INSERT INTO account_interest (id,name) VALUES (2,'Gardening');
+INSERT INTO account_interest (id,name) VALUES (3,'Numbers');
+INSERT INTO account_interest (id,name) VALUES (4,'Administration');
+INSERT INTO account_interest (id,name) VALUES (5,'Civil Work');
+INSERT INTO account_interest (id,name) VALUES (6,'Nursing');
+INSERT INTO account_interest (id,name) VALUES (7,'Human Rights');
