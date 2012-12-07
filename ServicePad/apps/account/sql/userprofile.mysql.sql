d28f0f5d376aaff139208ae37b0ffc37c76ecf56
Bryan Marty <bxm156@case.edu>
2012-12-07 17:13:17 -0500

    Add trigger to enforce proper null requirements for profiles

--- ServicePad/apps/account/sql/userprofile.mysql.sql
+++ ServicePad/apps/account/sql/userprofile.mysql.sql
@@ -11,3 +11,23 @@ BEGIN
 	SIGNAL SQLSTATE '45000'
 		SET MESSAGE_TEXT = "DELETE operation not allowed. Use the stored procedue: delete_user(user_id)"; --
 END;
+
+CREATE TRIGGER validate_profile
+BEFORE INSERT ON account_userprofile
+FOR EACH ROW
+BEGIN
+IF NEW.account_type = 0 THEN
+	IF NEW.gender IS NULL OR NEW.ethnicity IS NULL THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = "Please supply both gender and ethnicity", MYSQL_ERRNO = 1001; --
+	END IF; --
+END IF; --
+IF NEW.account_type = 1 THEN
+	IF NEW.organization_name IS NULL OR NEW.organization_address IS NULL OR NEW.organization_city IS NULL
+	OR NEW.organization_state IS NULL or NEW.organization_postalzip IS NULL OR NEW.organization_phone IS NULL
+	THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = "Please supply all organization information", MYSQL_ERRNO = 1001; --
+	END IF; --
+END IF; --
+END;
