d1cbdec14a8cd47aeff3f98a0ca26f1488be74c6
Bryan Marty <bxm156@case.edu>
2012-12-09 07:00:48 -0500

    Added Kevin's user data

--- ServicePad/apps/account/sql/userprofile.mysql.sql
+++ ServicePad/apps/account/sql/userprofile.mysql.sql
@@ -1,11 +1,15 @@
 INSERT INTO `auth_user` (`id`, `username`, `first_name`, `last_name`, `email`, `password`, `is_staff`, `is_active`, `is_superuser`, `last_login`, `date_joined`)
 VALUES
 	(1, 'bxm156', 'Bryan', 'Marty', 'bryan.marty@case.edu', '!', 0, 1, 0, '2012-12-08 15:32:45', '2012-12-07 18:22:16');
-
+INSERT INTO `auth_user` (`id`, `username`, `first_name`, `last_name`, `email`, `password`, `is_staff`, `is_active`, `is_superuser`, `last_login`, `date_joined`)
+VALUES
+	(2, 'kwr17', 'Kevin', 'Rossoll', 'kevin.rossoll@case.edu', '!', 0, 1, 0, '2012-12-08 21:34:01', '2012-12-08 21:33:59');
 INSERT INTO `account_userprofile` (`account_type`, `gender`, `ethnicity`, `major`, `graduating_class`, `address`, `city`, `state`, `postalzip`, `authentication`, `user_id`, `organization_name`, `organization_address`, `organization_city`, `organization_state`, `organization_postalzip`, `organization_phone`)
 VALUES
 	(0, 'm', 'w', 'Computer Engineering', '12', '4917 Pine Ridge Dr.', 'Wootser', 'OH', '44691', 1, 1, NULL, NULL, NULL, NULL, NULL, NULL);
-
+INSERT INTO `account_userprofile` (`account_type`, `gender`, `ethnicity`, `major`, `graduating_class`, `address`, `city`, `state`, `postalzip`, `authentication`, `user_id`, `organization_name`, `organization_address`, `organization_city`, `organization_state`, `organization_postalzip`, `organization_phone`)
+VALUES
+	(0, 'm', 'w', 'English', '13', 'Carlton Rd.', 'Cleveland Heights', 'Oh', '44106', 1, 2, NULL, NULL, NULL, NULL, NULL, NULL);
 CREATE PROCEDURE delete_user(IN user_id INT)
 BEGIN
 	UPDATE auth_user SET is_active='0' WHERE id = user_id; --
