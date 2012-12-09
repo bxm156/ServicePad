026ae343db9cd134455cfdefc5d1580d8af26c89
Bryan Marty <bxm156@case.edu>
2012-12-09 12:44:29 -0500

    Added approving enrollment, skills listed on event page

--- ServicePad/apps/events/urls.py
+++ ServicePad/apps/events/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('ServicePad.apps.events.views',
     (r'^(?P<event_id>[0-9]+)/join/team/?$', 'join', {'show_teams':True}),
     (r'^(?P<event_id>[0-9]+)/join/team/(?P<team_id>[0-9]+)/?$', 'join', {'show_teams':True}),
     (r'^(?P<event_id>[0-9]+)/admin/?$','admin'),
+    (r'^(?P<event_id>[0-9]+)/admin/approve/(?P<enrollment_id>[0-9]+)/?$','approve_enrollment'),
     (r'^create/?$','create'),
 	(r'^', 'list')
 )
