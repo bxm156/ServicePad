e91772ede9a16ace6a9db8fe22d8e5166a9b6d67
Bryan Marty <bxm156@case.edu>
2012-12-10 03:06:57 -0500

    SQL COMMENT and added a try except

--- ServicePad/apps/service/views.py
+++ ServicePad/apps/service/views.py
@@ -6,8 +6,25 @@ from ServicePad.apps.service.forms import ServiceReviewForm
 from django.shortcuts import get_object_or_404, render, redirect
 
 def review(request,enrollment_id):
-    se = ServiceEnrollment.objects.filter(pk=enrollment_id).values('event__owner_id','event_id','event__name','user_id','user__first_name','user__last_name',
+    """
+    Allows an organization to enter in a review for user
+    """
+    
+    #Get the ServiceEnrollment this review will be fore
+    """
+    SELECT `events_event`.`owner_id`, `service_serviceenrollment`.`event_id`, `events_event`.`name`, `service_serviceenrollment`.`user_id`,
+    T4.`first_name`, T4.`last_name`, `service_serviceenrollment`.`team_id`, `team_team`.`name`, `service_serviceenrollment`.`start`,
+    `service_serviceenrollment`.`end` FROM `service_serviceenrollment`
+    INNER JOIN `events_event` ON (`service_serviceenrollment`.`event_id` = `events_event`.`id`)
+    INNER JOIN `auth_user` T4 ON (`service_serviceenrollment`.`user_id` = T4.`id`)
+    LEFT OUTER JOIN `team_team` ON (`service_serviceenrollment`.`team_id` = `team_team`.`id`)
+    WHERE `service_serviceenrollment`.`id` = 2 
+    """
+    try:
+        se = ServiceEnrollment.objects.filter(pk=enrollment_id).values('event__owner_id','event_id','event__name','user_id','user__first_name','user__last_name',
                 'team_id','team__name','start','end')[0]
+    except:
+        return redirect("/account")
     if request.user.id != int(se['event__owner_id']):
         return redirect("/events/")
     record = ServiceRecord(event_id=se['event_id'],user_id=se['user_id'],team_id=se['team_id'])
