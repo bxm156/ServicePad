8dad8a180cca4bbbbb10fb7f5fad7436cd7375fd
Bryan Marty <bxm156@case.edu>
2012-12-10 04:21:37 -0500

    Intial SQL data

--- ServicePad/apps/team/sql/team.mysql.sql
+++ ServicePad/apps/team/sql/team.mysql.sql
@@ -3,4 +3,8 @@ AFTER INSERT ON team_team
 FOR EACH ROW
 BEGIN 
 INSERT INTO team_teammembership (member_id,team_id,join_date,invite) VALUES (NEW.admin_id,NEW.id,NEW.JOIN_DATE,0); --
-END; --
+END;
+
+INSERT INTO `team_team` (`id`, `name`, `join_date`, `admin_id`)
+VALUES
+	(1, 'PKT', '2012-12-09 16:10:45', 1);
