9e9ed60c56d0aabad56b164f0be0790e2d98c41c
Bryan Marty <bxm156@case.edu>
2012-12-10 02:03:45 -0500

    Profiles now SQL COMMENTED

--- ServicePad/apps/pages/views.py
+++ ServicePad/apps/pages/views.py
@@ -76,17 +76,73 @@ def public_profile(request,user_id):
         return organization_profile(request,user_id,user,profile)
 
 def volunteer_profile(request,user_id,user,profile):
+    #Get User's Availability
+    """
+    SELECT `account_availability`.`id`, `account_availability`.`user_id`, `account_availability`.`start`, `account_availability`.`end`
+    FROM `account_availability`
+    WHERE `account_availability`.`user_id` = 1 
+    """
     availability = Availability.objects.filter(user=user_id)
+    print availability.query.__str__()
+    
+    #Get the user's skills
+    """
+    SELECT `account_skill`.`name` FROM `account_hasskill`
+    INNER JOIN `account_skill` ON (`account_hasskill`.`skill_id` = `account_skill`.`id`)
+    WHERE `account_hasskill`.`user_id` = 1 
+    """
     skills = HasSkill.objects.filter(user=user_id).values('skill__name')
+    print skills.query.__str__()
+    
+    #Get the user's interests
+    """
+    SELECT `account_interest`.`name`, `account_hasinterest`.`level` FROM `account_hasinterest`
+    INNER JOIN `account_interest` ON (`account_hasinterest`.`interest_id` = `account_interest`.`id`)
+    WHERE `account_hasinterest`.`user_id` = 1 
+    """
     interests = HasInterest.objects.filter(user=user_id).values('interest__name','level')
+    print interests.query.__str__()
+    
+    #Get past completed events
+    """
+    SELECT `service_servicerecord`.`event_id`, `events_event`.`name`, `service_servicerecord`.`hours`, `service_servicerecord`.`review`
+    FROM `service_servicerecord` INNER JOIN `events_event` ON (`service_servicerecord`.`event_id` = `events_event`.`id`)
+    WHERE (`service_servicerecord`.`user_id` = 1  AND `service_servicerecord`.`end` <= 2012-12-10 01:56:02 )
+    """
     past_events = ServiceRecord.objects.filter(user=user_id,end__lte=datetime.now()).values('event_id','event__name','hours','review')
+    print past_events.query.__str__()
+    
+    #Get a random review to show
+    """
+    SELECT `events_event`.`owner_id`, `service_servicerecord`.`review`, `service_servicerecord`.`rating`,
+    `account_userprofile`.`organization_name` FROM `service_servicerecord`
+    INNER JOIN `events_event` ON (`service_servicerecord`.`event_id` = `events_event`.`id`)
+    INNER JOIN `auth_user` T4 ON (`events_event`.`owner_id` = T4.`id`)
+    LEFT OUTER JOIN `account_userprofile` ON (T4.`id` = `account_userprofile`.`user_id`)
+    WHERE (`service_servicerecord`.`user_id` = 1  AND LENGTH(review) >= 5) ORDER BY RAND() LIMIT 1
+    """
     review = ServiceRecord.objects.filter(user=user_id).extra(where=['LENGTH(review) >= 5']).values('event__owner','review','rating','event__owner__userprofile__organization_name').order_by('?')[:1]
-    total_hours = ServiceRecord.objects.filter(user=user_id).aggregate(total_hours=Sum('hours'))
-    print total_hours
+    print review.query.__str__()
     if review:
         review = review[0]
+
+    #Get the total number of servie hours the user performed
+    """
+    SELECT SUM(`service_servicerecord`.`hours`) AS `total_hours`
+    FROM `service_servicerecord` WHERE `service_servicerecord`.`user_id` = 1
+    """
+    total_hours = ServiceRecord.objects.filter(user=user_id).aggregate(total_hours=Sum('hours'))
+    
+
+    #Get the teams the user is involved in
+    """
+    SELECT `team_team`.`id`, `team_team`.`name`, `team_teammembership`.`join_date` FROM `team_team`
+    LEFT OUTER JOIN `team_teammembership` ON (`team_team`.`id` = `team_teammembership`.`team_id`)
+    WHERE `team_teammembership`.`member_id` = 1 
+    """
     teams = Team.objects.filter(members=user_id).values('id','name','teammembership__join_date')
-    print review
+    print teams.query.__str__()
+
     context = {
                'user':user,
                'profile':profile,
@@ -102,12 +158,29 @@ def volunteer_profile(request,user_id,user,profile):
     return render(request,'public_profile_volunteer.djhtml',context)
 
 def organization_profile(request,user_id,user,profile):
+    
+    #Get all the organization's upcoming events
+    """
+    SELECT `events_event`.`id`, `events_event`.`name`, `events_event`.`start_time`, `events_event`.`end_time`
+    FROM `events_event` WHERE (`events_event`.`owner_id` = 6  AND `events_event`.`start_time` > 2012-12-10 02:01:12 )
+    """
     upcoming_events = Event.objects.filter(owner=user_id,start_time__gt=datetime.now()).values('id','name','start_time','end_time')
+    print upcoming_events.query.__str__()
+
+    #Get all the organization's current events
+    """
+    SELECT `events_event`.`id`, `events_event`.`name`, `events_event`.`start_time`, `events_event`.`end_time` FROM `events_event` WHERE (`events_event`.`owner_id` = 6  AND `events_event`.`start_time` <= 2012-12-10 02:01:12  AND `events_event`.`end_time` > 2012-12-10 02:01:12 )
+    """
     current_events = Event.objects.filter(owner=user_id,start_time__lte=datetime.now(),end_time__gt=datetime.now()).values('id','name','start_time','end_time')
+    print current_events.query.__str__()
+
+    #Get all the organization's past events
+    """
+    SELECT `events_event`.`id`, `events_event`.`name`, `events_event`.`start_time`, `events_event`.`end_time` FROM `events_event` WHERE (`events_event`.`owner_id` = 6  AND `events_event`.`end_time` <= 2012-12-10 02:01:12 )
+    """
     past_events = Event.objects.filter(owner=user_id,end_time__lte=datetime.now()).values('id','name','start_time','end_time')
-    print upcoming_events
-    print current_events
-    print past_events
+    print past_events.query.__str__()
+    
     context = {'profile':profile,'user':user,'upcoming_events':upcoming_events,'current_events':current_events,'past_events':past_events}
     return render(request,'public_profile_organization.djhtml',context)
     
