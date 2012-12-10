bd48fe606adc4b161a221dd157a58760c103c148
Bryan Marty <bxm156@case.edu>
2012-12-10 06:35:12 -0500

    Lots of inital data

--- ServicePad/apps/account/sql/availability.mysql.sql
+++ ServicePad/apps/account/sql/availability.mysql.sql
@@ -1,3 +1,12 @@
+INSERT INTO `account_availability` (`id`, `user_id`, `start`, `end`)
+VALUES
+	(6, 2, '08:00:00', '11:00:00'),
+	(1, 8, '17:00:00', '21:00:00'),
+	(4, 9, '11:30:00', '13:45:00'),
+	(5, 9, '17:00:00', '21:00:00'),
+	(2, 10, '09:00:00', '13:00:00'),
+	(3, 10, '15:00:00', '19:00:00');
+
 CREATE TRIGGER validate_availability_start_end
 BEFORE INSERT ON account_availability
 FOR EACH ROW
---
+++ ServicePad/apps/messages/sql/message.mysql.sql
@@ -0,0 +1,8 @@
+INSERT INTO `messages_message` (`id`, `toUser_id`, `fromUser_id`, `subject`, `message`, `date_sent`)
+VALUES
+	(1, 1, 8, 'Hey', 'Hey, would you be interested in joining me for a service event?', '2012-12-10 04:49:18'),
+	(2, 8, 9, 'Hi, Jim', 'You\'re too slim.', '2012-12-10 04:53:39'),
+	(3, 10, 9, 'Help the juveniles', 'Mary,\r\n\r\nPlease help me help Jane help the juveniles get jungle gyms.\r\n\r\nCharles Blarles', '2012-12-10 04:55:33'),
+	(4, 9, 10, 'I will', 'Mary,\r\n\r\nI will help you. \r\n\r\nCharles.', '2012-12-10 04:57:20'),
+	(5, 10, 2, 'Architecting', 'Mr. Blarles,\r\n\r\nWe should architect some architecture.\r\n\r\nKevin', '2012-12-10 04:59:22'),
+	(6, 6, 8, 'Database Info', 'Hi,\r\n\r\nI would like to get some more information about databases.', '2012-12-10 05:00:31');
